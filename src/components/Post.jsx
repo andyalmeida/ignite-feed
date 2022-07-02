670e97058a2ba1fd011f321711ff4be6f82d3bfd
André Almeida <andalvalmeida@gmail.com>
2022-07-02 14:46:22 -0300

    feat: create Post component :sparkles:

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/andyalmeida.png"/>
+          <div className={styles.authorInfo}>
+            <strong>André Almeida</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="02 de Julho de 2022 às 12:04h" dateTime="2022-07-02 12:04:52">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
