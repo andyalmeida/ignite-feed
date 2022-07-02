2dbe15ebba7574fe175d49fbd06ff0d9cfcac024
André Almeida <andalvalmeida@gmail.com>
2022-07-02 18:04:46 -0300

    feat: create Comment component :sparkles:

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { HandsClapping, Trash } from "phosphor-react";
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/andyalmeida.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>André Almeida</strong>
+              <time title="02 de Julho de 2022 às 12:04h" dateTime="2022-07-02 12:04:52">Cerca de 2h atrás</time>
+            </div>
+           <button title="Deletar comentário">
+             <Trash size={24}/>
+           </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <HandsClapping />
+            Aplaudir <span>24</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
