9ae2dec0565aaa43d038060f8639d89d956c9b1f
André Almeida <andalvalmeida@gmail.com>
2022-07-02 09:37:09 -0300

    feat: create Sidebar component :sparkles:

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilSimpleLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1655798881492-f4e778a79b50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx88&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/andyalmeida.png"
+        />
+
+        <strong>André Almeida</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
